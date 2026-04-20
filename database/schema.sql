70ecc27876da6aea5fad0fd1acf18d100dbf3a4d
Christopher Ahn <chrisahn90@protonmail.com>
2026-04-20 23:02:02 +0000

    created database and table

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,14 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table if not exists "users" (
+  users_id INT,
+  username VARCHAR(255),
+  email VARCHAR(255),
+  hashpassword VARCHAR(255),
+
+  cart JSONB,
+  shoplist JSONB,
+  purchaselist JSONB
+);
