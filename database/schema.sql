0b51db16915cee1e5297455fb8a766ec12a532be
Christopher Ahn <chrisahn90@protonmail.com>
2026-05-08 10:08:03 +0000

    Added another table purchase_item and added more api requests but still need testing

--- database/schema.sql
+++ database/schema.sql
@@ -18,3 +18,20 @@ create table if not exists "users" (
   shoplist JSONB DEFAULT '[]'::jsonb,
   purchaselist JSONB DEFAULT '[]'::jsonb
 );
+
+create table if not exists "purchases" (
+  purchase_id UUID NOT NULL PRIMARY KEY,
+  users_id INT REFERENCES users(users_id) ON DELETE CASCADE,
+  total_amount DECIMAL(10, 2),
+  user_address VARCHAR(255),
+  user_card JSONB,
+  purchase_date VARCHAR(255)
+);
+
+create table if not exists "purchase_items" (
+  item_id SERIAL NOT NULL PRIMARY KEY,
+  purchase_id UUID REFERENCES purchases(purchase_id) ON DELETE CASCADE,
+  product_id INT,
+  quantity INT,
+  price DECIMAL(10, 2)
+);
