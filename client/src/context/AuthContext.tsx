5e5341e56b96a4975ba87e46847703d183972fec
Christopher Ahn <chrisahn90@protonmail.com>
2026-06-25 00:50:23 +0000

    Set up AuthContext but still needs more work

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,50 @@
+// import React, {
+//   useEffect,
+//   useState,
+//   useContext,
+//   createContext,
+//   useLayoutEffect,
+//   useRef,
+// } from 'react';
+
+import {
+  useEffect,
+  useState,
+  useContext,
+  createContext,
+  ReactNode,
+} from 'react';
+
+// import { jwtDecode } from 'jwt-decode';
+// import UseRefreshToken from '../hooks/useRefreshToken';
+// import { url } from '../configURL/configURL';
+
+export const AuthContext = createContext({});
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [accessToken, setAccessToken] = useState({});
+
+  const [currentUsername, setCurrentUsername] = useState('');
+  const [currentUserID, setCurrentUserID] = useState('');
+
+  // const [tokenExp, setTokenExp] = useState(null);
+
+  useEffect(() => {}, []);
+  return (
+    <AuthContext.Provider
+      value={{
+        accessToken,
+        setAccessToken,
+        currentUsername,
+        setCurrentUsername,
+        currentUserID,
+        setCurrentUserID,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
