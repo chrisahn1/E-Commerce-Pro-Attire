dde772b3a672db49e61d822c5762a124359523ac
Christopher Ahn <chrisahn90@protonmail.com>
2026-09-07 22:12:08 +0000

    changing login and signup into navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 // import { useEffect, useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
+// import Home from './pages/Home';
+// import Signup from './pages/Signup';
 import Layout from './components/Layout';
-import Userpage from './pages/Userpage';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import TopWear from './pages/TopWear';
 import Pants from './pages/Pants';
 import Shoes from './pages/Shoes';
@@ -11,7 +13,7 @@ import PurchaseList from './pages/PurchaseList';
 import Kart from './pages/Kart';
 import Settings from './pages/Settings';
 
-import ProtectRoute from './routes/ProtectedRoutes';
+// import ProtectRoute from './routes/ProtectedRoutes';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
@@ -22,7 +24,18 @@ export default function App() {
       <div className="App">
         <HashRouter>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/topwear" element={<TopWear />} />
+              <Route path="/pants" element={<Pants />} />
+              <Route path="/shoes" element={<Shoes />} />
+              <Route path="/purchaselist" element={<PurchaseList />} />
+              <Route path="/kart" element={<Kart />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            {/* <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
             <Route element={<ProtectRoute />}>
               <Route element={<Layout />}>
@@ -34,7 +47,7 @@ export default function App() {
                 <Route path="/kart" element={<Kart />} />
                 <Route path="/settings" element={<Settings />} />
               </Route>
-            </Route>
+            </Route> */}
           </Routes>
         </HashRouter>
       </div>
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,75 @@
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { jwtDecode } from 'jwt-decode';
+import { useAuth } from '../context/AuthContext';
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const { setAccessToken } = useAuth();
+
+  const [email_input, setEmail] = useState('');
+  const [password_input, setPassword] = useState('');
+
+  const handleEmail = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const body = {
+        email: email_input,
+        password: password_input,
+      };
+      // 'http://127.0.0.1:5173/api/login'
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+      const result = await response.json();
+      // const decoded_token = jwtDecode(result.accessToken);
+      // console.log(decoded_token);
+      setAccessToken(result.accessToken);
+      navigate('/');
+    } catch (error) {
+      console.error('Network error:', error);
+    }
+  };
+  return (
+    <div>
+      <div>Login</div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email_input}
+            placeholder="Email.."
+            onChange={handleEmail}
+            required
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            value={password_input}
+            placeholder="Password.."
+            onChange={handlePassword}
+            required
+          />
+          <button className="submitButton" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
